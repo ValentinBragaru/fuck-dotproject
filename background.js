3939476b66a05425f19e71e5e0d04c2e55ff1199
Valentin Bragaru <valentin.bragaru@gmail.com>
2013-07-10 22:12:44 +0300

    First Commit

---
+++ background.js
@@ -0,0 +1,6 @@
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.create({'url': chrome.extension.getURL('background.html')}, function(tab) {
+    // Tab opened.
+  });
+});
